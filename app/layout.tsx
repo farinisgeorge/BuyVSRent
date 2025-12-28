a4db4404f93ed94cc0e17f7376dead14eb1b9994
farinisgeorgios <farinisgeorgios@gmail.com>
2025-12-29 00:42:56 +0200

    import vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { AuthProvider } from "@/src/contexts/AuthContext";
 import { Header } from "@/src/components/Header";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
